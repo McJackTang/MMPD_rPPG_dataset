8488cd209fe31dfc006ec5cdd8a2326fd4560de1
Jack Tang <73820234+McJackTang@users.noreply.github.com>
2023-03-03 19:17:51 +0800

    Update __init__.py

--- rPPG-Toolbox_upload/dataset/data_loader/__init__.py
+++ rPPG-Toolbox_upload/dataset/data_loader/__init__.py
@@ -4,5 +4,6 @@ import dataset.data_loader.UBFCLoader
 import dataset.data_loader.PURELoader
 import dataset.data_loader.SCAMPSLoader
 import dataset.data_loader.MMPDLoader
+import dataset.data_loader.process_tool
 
 
