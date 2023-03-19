511fbff7df80c53af2e93fceef90397032b6046b
AAAAAAbaAba <71625927+AAAAAAbaAba@users.noreply.github.com>
2023-03-19 11:20:33 +0800

    Delete rppg-Toolbox_MMPD directory

--- rppg-Toolbox_MMPD/neural_methods/trainer/__init__.py
+++
@@ -1,5 +0,0 @@
-import neural_methods.trainer.BaseTrainer
-import neural_methods.trainer.PhysnetTrainer
-import neural_methods.trainer.TscanTrainer
-import neural_methods.trainer.DeepPhysTrainer
-import neural_methods.trainer.EfficientPhysTrainer
